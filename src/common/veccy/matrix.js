cbf1e3d7cd127006dc4c65af4ccb9510363da1d5
copygirl <halfnerd.copygirl@gmail.com>
2016-03-12 04:01:40 +0100

    Add Matrix.frustum, .perspective and .ortho

--- src/common/veccy/matrix.js
+++ src/common/veccy/matrix.js
@@ -72,10 +72,46 @@ module.exports = class Matrix {
     m.setRow(2, ...repeat(z, 3));
     
     let t = Matrix.identity(4);
-    t.setColumn(3, eye.negate());
+    t.setColumn(3, ...eye.negate());
     
     return m.multiply(t);
   }
+  
+  static frustum(left, right, bottom, top, znear, zfar) {
+    let X = 2 * znear / (right - left);
+    let Y = 2 * znear / (top - bottom);
+    let A = (right + left) / (right - left);
+    let B = (top + bottom) / (top - bottom);
+    let C = -(zfar + znear) / (zfar - znear);
+    let D = -2 * zfar * znear / (zfar - znear);
+    
+    return Matrix.create(
+      [[ X, 0,  A, 0 ]
+       [ 0, Y,  B, 0 ]
+       [ 0, 0,  C, D ]
+       [ 0, 0, -1, 0 ]]);
+  }
+  
+  static perspective(fovy, aspect, znear, zfar) {
+    let ymax = znear * Math.tan(fovy * Math.PI / 360.0);
+    let ymin = -ymax;
+    let xmin = ymin * aspect;
+    let xmax = ymax * aspect;
+    
+    return Matrix.frustum(xmin, xmax, ymin, ymax, znear, zfar);
+  };
+  
+  static ortho(left, right, bottom, top, znear, zfar) {
+    let tx = -(right + left) / (right - left);
+    let ty = -(top + bottom) / (top - bottom);
+    let tz = -(zfar + znear) / (zfar - znear);
+    
+    return Matrix.create(
+      [[ 2 / (right - left), 0, 0, 0 ]
+      [ 0, 2 / (top - bottom), 0, 0 ]
+      [ 0, 0, -2 / (zfar - znear), 0 ]
+      [ tx, ty, tz, 1 ]]);
+  };
   
   
   get size() { return [ this.columns, this.rows ]; }
