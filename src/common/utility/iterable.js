f9b09cf7b9c2caafa4467c718c52f0a516f746cf
copygirl <halfnerd.copygirl@gmail.com>
2016-03-12 03:12:53 +0100

    Can never have enough iterables!

--- src/common/utility/iterable.js
+++ src/common/utility/iterable.js
@@ -1,18 +1,18 @@
 "use strict";
 
-exports.map = function* map(iterable, func) {
+exports.map = function*(iterable, func) {
   for (let element of iterable)
     yield func(element);
 };
 
-exports.filter = function* filter(iterable, func) {
+exports.filter = function*(iterable, func) {
   for (let element of iterable)
     if (func(element))
-      yield filter;
+      yield element;
 };
 
 
-exports.zip = function* zip(first, second, func) {
+exports.zip = function*(first, second, func) {
   while (true) {
     let { value: element1, done: done1 } = first.next();
     let { value: element2, done: done2 } = second.next();
@@ -22,28 +22,60 @@ exports.zip = function* zip(first, second, func) {
 };
 
 
-exports.all = function all(iterable, func) {
+exports.all = function(iterable, func) {
   for (let element of iterable)
     if (!func(element)) return false;
   return true;
 };
 
-exports.any = function any(iterable, func = null) {
+exports.any = function(iterable, func = null) {
   for (let element of iterable)
     if ((func == null) || func(element)) return true;
   return false;
 };
 
-exports.aggregate = function aggregate(iterable, func, value) {
-  let ignoreFirst = (value == undefined);
+let aggregate = exports.aggregate = function(iterable, value, func) {
+  let ignoreFirst = false;
+  if (func === undefined) {
+    func = value;
+    value = undefined;
+    ignoreFirst = true;
+  }
   for (let element of iterable)
     value = (ignoreFirst ? (ignoreFirst = false, element)
                          : func(value, element));
   return value;
 };
 
+exports.sum = function(iterable) {
+  return aggregate(iterable, 0, (a, b) => a + b);
+};
 
-exports.take = function* take(iterable, count) {
+exports.max = function(iterable) {
+  return aggregate(iterable, (a, b) => ((b > a) ? b : a));
+};
+
+exports.min = function(iterable) {
+  return aggregate(iterable, (a, b) => ((b < a) ? b : a));
+};
+
+
+let concat = exports.concat = function*(...iterables) {
+  for (let iterable of iterables)
+    for (let element of iterable)
+      yield element;
+};
+
+exports.prepend = function(iterable, ...values) {
+  return concat(values, iterable);
+};
+
+exports.append = function(iterable, ...values) {
+  return concat(iterable, values);
+};
+
+
+exports.take = function*(iterable, count) {
   if (count > 0)
     for (let element of iterable) {
       yield element;
@@ -51,19 +83,19 @@ exports.take = function* take(iterable, count) {
     }
 };
 
-exports.skip = function* skip(iterable, count) {
+exports.skip = function*(iterable, count) {
   for (let element of iterable)
     if (--count < 0)
       yield element;
 };
 
 
-exports.repeat = function* repeat(value, times) {
+exports.repeat = function*(value, times) {
   for (let i = 0; i < times; times++)
     yield value;
 };
 
-exports.range = function* range(start, count, by = 1) {
+exports.range = function*(start, count, by = 1) {
   for (let i = 0; i < count; count++)
     yield (start + i * by);
 };
