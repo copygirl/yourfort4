86540beeb3ed251c4129570b65d9eb882d47f48e
copygirl <halfnerd.copygirl@gmail.com>
2016-06-25 20:26:12 +0200

    Update Player, basic onGround

--- src/common/Player.js
+++ src/common/Player.js
@@ -18,15 +18,25 @@ module.exports = class Player extends Entity {
     this.collider = "box";
     
     this.movement = { left: false, right: false, jump: false };
+    
+    this.onGround = false;
   }
   
-  // TODO
-  get onGround() { return false; }
   
   update(delta) {
-    this.speed = this.speed.add((this.movement.right - this.movement.left) * MOVE_SPEED, 0);
+    let box = {
+      minX: this.pos[0] - (this.size[0] / 2) + 1,
+      maxX: this.pos[0] + (this.size[0] / 2) - 1,
+      minY: this.pos[1] + (this.size[1] / 2) - 1,
+      maxY: this.pos[1] + (this.size[1] / 2) + 1
+    };
+    this.onGround = this.main.physics.collision.entitiesInBBox(box)
+      .filter(e => (e.solid && (e !== this)))
+      .any();
+    
+    this.speed = [ (this.movement.right - this.movement.left) * MOVE_SPEED, this.speed[1] ];
     if (this.movement.jump && this.onGround)
-      this.speed = this.speed.add(0, -JUMP_SPEED);
+      this.speed = [ this.speed[0], -JUMP_SPEED ];
   }
   
 };
